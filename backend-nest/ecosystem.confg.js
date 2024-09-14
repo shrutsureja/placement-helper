5062ab610bcd6d22d1a041e58c3dfec6a975517b
Shrut Sureja <shrutsureja.code@gmail.com>
2024-09-14 16:17:15 +0530

    Create ecosystem.confg.js

---
+++ backend-nest/ecosystem.confg.js
@@ -0,0 +1,11 @@
+{
+  apps : [
+    {
+      name      : "backend-node-prod",
+      namespace : "node prod",
+      script    : "npm run start:prod",
+      interpreter: "none",
+      args: ""
+    }
+  ]
+}
